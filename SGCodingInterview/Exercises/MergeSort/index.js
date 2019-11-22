2f73178968da543b1ffc1e74237cdbd9cb081c8c
jonathanferreras <jonathanferreras@icloud.com>
2019-11-22 11:26:05 -0500

    mergesort: impl

--- SGCodingInterview/Exercises/MergeSort/index.js
+++ SGCodingInterview/Exercises/MergeSort/index.js
@@ -7,11 +7,20 @@
 
 
 function mergeSort(arr) {
+  if(arr.length === 1) {
+    return arr;
+  }
 
+  const midpoint = Math.floor(arr.length / 2);
+  const left = arr.slice(0, midpoint);
+  const right = arr.slice(midpoint);
+
+  return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
   let results = [];
+
   while(left.length > 0 && right.length > 0) {
     if(left[0] < right[0]) {
       results.push(left.shift());
