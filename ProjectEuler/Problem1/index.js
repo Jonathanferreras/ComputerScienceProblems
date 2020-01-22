5de5e2a9cd0297eac2e0c263356a85ee183260ec
Jonathan Ferreras <jonathanferreras@icloud.com>
2020-01-22 16:49:41 -0500

    projectEuler: solved and tested problem 1

---
+++ ProjectEuler/Problem1/index.js
@@ -0,0 +1,11 @@
+module.exports = function multipleSum(num) {
+  let sum = 0;
+
+  for(let i = 0; i < num; i++) {
+    if ((i % 3 == 0) || (i % 5 === 0)) {
+      sum += i;
+    }
+  }
+
+  return sum;
+}
